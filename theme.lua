9d25e4aef5c55228a17b43626d8403ee284ceb7a
bluzukk <d5gu2s@gmail.com>
2020-08-02 01:53:43 +0200

    Here some code

--- theme.lua
+++ theme.lua
@@ -8,7 +8,7 @@ local wibox      = require("wibox")
 local beautiful  = require("beautiful")
 local gears      = require("gears")
 local lain       = require("lain")
-local naughty    = require ("naughty")
+local naughty    = require("naughty")
 local markup     = lain.util.markup
 local helpers    = require("lain.helpers")
 local xresources = require("beautiful.xresources")
@@ -31,92 +31,70 @@ cmd_gpu       = 'bash -c "nvidia-smi"'
 cmd_proc_cpu  = 'bash -c "ps -Ao pcpu,comm,user,pid, --sort=-pcpu | head -n 30"'
 cmd_proc_mem  = 'bash -c "ps -Ao pmem,comm,user,pid --sort=-pmem | head -n 30"'
 
-cmd_wttr      = 'bash -c "curl wttr.in"'
-
 cmd_net_info  = 'bash -c "~/.config/awesome/Scripts/net_info.sh"'
 cmd_net_total = 'bash -c "~/.config/awesome/Scripts/net_totals.sh"'
 cmd_mail_ims  = 'bash -c "~/.config/awesome/Scripts/Hidden/checkmail_ims.sh"'
 cmd_mail      = 'bash -c "~/.config/awesome/Scripts/Hidden/checkmail_uni.sh"'
 
-
 -- Colors
 local black        = "#000000"
 local white        = "#FFFFFF"
-local gray         = "#94928F"
-local blue         = "#4169E1"
-local green        = "#00CC66"
-local light_green  = "#46FF9B"
-local green_orange = "#87af5f"
-local yellow       = "#FFFF00"
-local orange       = "#FFA500"
 local red          = "#e54c62"
-local reds         = "#FF5555"
-local pink         = "#FF45A8"
-local purple       = "#800080"
-local purple_dark  = "#461B7E"
-local purple_wall  = "#774A86"
 
-beautiful.widget_main_color = "#4169E1"
-beautiful.widget_red   = red
-beautiful.widget_blue  = blue
-beautiful.widget_green = green
-
-local color_default  = xrdb.color6
-local color_moderate = xrdb.color6
-local color_stress   = xrdb.color6
-local color_critical = red
-local accent_color   = xrdb.color2
-local main_color     = xrdb.color3
+local color_default    = xrdb.color6
+local color_moderate   = xrdb.color7
+local color_stress     = xrdb.color8
+local color_critical   = red
+local accent_color     = xrdb.color2
+local main_color       = xrdb.color3
+local background_color = xrdb.background
 
 -- local std_font = "Terminus (TTF) 15"
 -- local std_font = "Mono 13"
 local std_font = "Sans 13"
 
 theme.font                  = std_font
-theme.fg_normal             = white
-theme.fg_focus              = black
+theme.fg_normal             = xrdb.color2
 theme.bg_normal             = black
-theme.bg_focus              = green
 theme.fg_urgent             = black
 theme.bg_urgent             = xrdb.color10
-theme.border_normal         = xrdb.background
+theme.border_normal         = background_color
 theme.border_focus          = xrdb.color5
 theme.taglist_fg_focus      = xrdb.color6
-theme.taglist_bg_focus      = xrdb.background
+theme.taglist_bg_focus      = background_color
 theme.tasklist_fg_focus     = xrdb.color6
 theme.tasklist_fg_normal    = xrdb.color2
-theme.tasklist_bg_focus     = xrdb.background
-theme.tasklist_bg_normal    = xrdb.background
+theme.tasklist_bg_focus     = background_color
+theme.tasklist_bg_normal    = background_color
 theme.border_width          = 0
 theme.tasklist_disable_icon = true
 theme.useless_gap           = 8
 -- theme.tasklist_plain_task_name = true
 
+local notification = nil
+
 function notification_hide()
     if not notification then return end
-    naughty.destroy(notification)
-    notification = nil
+    -- naughty.destroy(notification)
+    -- notification = nil
 end
 
 function notification_show(str)
-    notification_hide()
+    -- notification_hide()
     notification = naughty.notify {
         preset = {
             font = "Terminus (TTF) 18",
             fg = main_color,
             bg = black
           },
-        -- title  = "Warning",
+        --title  = "Warning",
         text   = str
     }
 end
 
 -- Textclock
-local mytextclock = wibox.widget.textclock(" %H%M ")
-mytextclock.font = theme.font
-
-
-local pomodoro = require("widgets.pomodoro.pomodoro")
+local textclock = wibox.widget.textclock(" %H%M ")
+textclock.font = theme.font
 
 --Calendar
 lain.widget.cal({
@@ -137,7 +115,7 @@ function net_notification_show(str)
     end)
 end
 ip4 = awful.widget.watch(cmd_ip4, 600, function(widget, stdout)
-    if (stdout == "") then
+    if not stdout then
         widget:set_markup(markup.fontfg(std_font, accent_color, 'no ip4'))
     else
         widget:set_markup(markup.fontfg(std_font, accent_color, stdout))
@@ -222,40 +200,35 @@ function cpu_notification_show(str)
         notification_show(f)
     end)
 end
-local runonce = 0
-function cpu_on_click()
-    awful.spawn(terminal .. ' htop -s PERCENT_CPU')
-    -- TODO Fix
-    helpers.async('sleep 1', function(f)
-        runonce = 0
-    end)
-end
+-- local runonce = 0
+-- function cpu_on_click()
+--     awful.spawn(terminal .. ' htop -s PERCENT_CPU')
+--     -- TODO Fix
+--     helpers.async('sleep 1', function(f)
+--         runonce = 0
+--     end)
+-- end
 local cpu_util = lain.widget.cpu({
     settings = function()
         local value = cpu_now.usage
-        if value > 65 then
-            color = color_critical
-        elseif value > 47 then
-            color = color_stress
-        elseif value > 27 then
-            color = color_moderate
-        elseif value > 0 then
-            color = color_default
+        if     value > 65 then color = color_critical
+        elseif value > 47 then color = color_stress
+        elseif value > 27 then color = color_moderate
+        else                   color = color_default
         end
         widget:set_markup(markup.font(std_font, markup(color, cpu_now.usage .. "%")))
-        -- widget:connect_signal("mouse::enter", cpu_notification_show)
+        widget:connect_signal("mouse::enter", cpu_notification_show)
         widget:connect_signal("mouse::leave", notification_hide)
-        widget:connect_signal("button::press", function(_, _, _, button)
-            if (button == 3) then cpu_notification_show()
-            elseif (button == 1) then cpu_notification_show()
-            elseif (button == 2) then
-              if (runonce == 0) then
-                runonce = 1
-                cpu_on_click()
-              end
-            end
-        end)
-
+        -- widget:connect_signal("button::press", function(_, _, _, button)
+        --     if (button == 3) then cpu_notification_show()
+        --     elseif (button == 1) then cpu_notification_show()
+        --     elseif (button == 2) then
+        --       if (runonce == 0) then
+        --         runonce = 1
+        --         cpu_on_click()
+        --       end
+        --     end
+        -- end)
     end
 })
 
@@ -265,39 +238,35 @@ function mem_notification_show(str)
         notification_show(f)
     end)
 end
-local runonce = 0
-function mem_on_click()
-    awful.spawn(terminal .. ' htop -s PERCENT_MEM')
-    -- TODO Fix
-    helpers.async('sleep 1', function(f)
-        runonce = 0
-    end)
-end
+-- local runonce = 0
+-- function mem_on_click()
+--     awful.spawn(terminal .. ' htop -s PERCENT_MEM')
+--     -- TODO Fix
+--     helpers.async('sleep 1', function(f)
+--         runonce = 0
+--     end)
+-- end
 local mem = lain.widget.mem({
     settings = function()
         local value = mem_now.used
-        if value > 13000 then
-            color = color_critical
-        elseif value > 9000 then
-            color = color_stress
-        elseif value > 6000 then
-            color = color_moderate
-        elseif value > 0 then
-            color = color_default
+        if     value > 13000 then color = color_critical
+        elseif value > 9000  then color = color_stress
+        elseif value > 6000  then color = color_moderate
+        else                      color = color_default
         end
         widget:set_markup(markup.font(std_font, markup(color, value .. "mb")))
-        -- widget:connect_signal("mouse::enter", mem_notification_show)
+        widget:connect_signal("mouse::enter", mem_notification_show)
         widget:connect_signal("mouse::leave", notification_hide)
-        widget:connect_signal("button::press", function(_, _, _, button)
-            if (button == 3) then mem_notification_show()
-            elseif (button == 1) then mem_notification_show()
-            elseif (button == 2) then
-              if (runonce == 0) then
-                runonce = 1
-                mem_on_click()
-              end
-            end
-        end)
+        -- widget:connect_signal("button::press", function(_, _, _, button)
+        --     if     (button == 3) then mem_notification_show()
+        --     elseif (button == 1) then mem_notification_show()
+        --     elseif (button == 2) then
+        --       if (runonce == 0) then
+        --         runonce = 1
+        --         mem_on_click()
+        --       end
+        --     end
+        -- end)
     end
 })
 
@@ -318,19 +287,12 @@ end)
 local net = lain.widget.net({
     settings = function()
         value = tonumber(net_now.received)
-        if value > 3200 then
-            color = color_critical
-        elseif value > 1800 then
-            color = color_stress
-        elseif value > 400 then
-            color = color_moderate
-        else
-            color = color_default
-        end
+        if     value > 3200 then color = color_critical
+        elseif value > 1800 then color = color_stress
+        elseif value > 400  then color = color_moderate
+        else                     color = color_default end
         value = string.format("%04.0f", value)
-        widget:set_markup(
-            markup.fontfg(std_font, color, " " .. value .. "kb/s ")
-          )
+        widget:set_markup(markup.fontfg(std_font, color, " " .. value .. "kb/s "))
     end
 })
 
@@ -340,35 +302,28 @@ local bat = lain.widget.bat({
     settings = function()
         value = tonumber(bat_now.perc)
         if value then
-            if value > 59 then
-              color = color_default
-            elseif value > 46 then
-              color = color_moderate
-            elseif value > 24 then
-              color = color_stress
-            else
-              color = color_critical
-            end
-            widget:set_markup(markup.font(std_font, markup(color, value .. "%")))
+            if     value > 59 then color = color_default
+            elseif value > 46 then color = color_moderate
+            elseif value > 24 then color = color_stress
+            else                   color = color_critical end
+            widget:set_markup(
+                markup.font(std_font, markup(color, value .. "%"))
+            )
         end
-    end
+end
 })
 
 --Weather
-function weather_on_click()
-    awful.spawn(terminal .. ' curl && wttr.in')
-end
 theme.weather = lain.widget.weather({
     notification_preset = { font = "Terminus (TTF) 18", fg = main_color},
-    city_id = 2836320,
-    icons_path = '',
+    city_id = 2836320, icons_path = '',
     settings = function()
         units = math.floor(weather_now["main"]["temp"])
         widget:set_markup(markup.fontfg(std_font, accent_color, units .. "°C"))
-        widget:connect_signal("button::press", weather_on_click)
     end
 })
 
+-- local pomodoro = require("widgets.pomodoro.pomodoro")
 local spr = wibox.widget.textbox('   ')
 
 function theme.at_screen_connect(s)
@@ -384,58 +339,37 @@ function theme.at_screen_connect(s)
     s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, awful.util.tasklist_buttons)
     -- Create the wibox
     -- s.mywibox = awful.wibar({ position = "top", screen = s, bg = beautiful.bg_normal .. "0", height = 20  })
-    s.mywibox = awful.wibar({ position = "top", screen = s, bg = xrdb.background, height = 20 })
+    s.mywibox = awful.wibar({ position = "top", screen = s, bg = background_color, height = 20 })
 
     -- Add widgets to the wibox
     s.mywibox:setup {
         layout = wibox.layout.align.horizontal,
         { -- Left widgets
             layout = wibox.layout.fixed.horizontal,
-            s.mytaglist, s.mytxtlayoutbox,
-            spr,
+            s.mytaglist, s.mytxtlayoutbox, spr,
             net.widget,
-            total_net,
-            spr,
-            ip4,
-            spr,
-            ssid,
-            spr,
-            s.mypromptbox,
-            spr,
+            total_net, spr,
+            ip4, spr,
+            ssid, spr,
+            s.mypromptbox, spr,
         },
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            -- spr,
-            --todo_widget,
-            cpu_util,
-            spr,
-            cpu_temp,
-            spr,
-            gpu_temp,
-            spr,
+            --todo_widget, spr,
+            cpu_util, spr,
+            cpu_temp, spr,
+            gpu_temp, spr,
             mem.widget,
-            mail,
-            mail_ims,
-            bat.widget,
-            spr,
-            theme.weather.widget,
-            spr,
-            theme.fs.widget,
-            spr,
-            pomodoro,
-            spr,
-            wibox.widget.systray(),
-            spr,
-            mytextclock,
+            mail, mail_ims,
+            bat.widget, spr,
+            theme.weather.widget,spr,
+            theme.fs.widget, spr,
+            --pomodoro, spr,
+            wibox.widget.systray(), spr,
+            textclock,
         },
     }
 end
 
 return theme
-
--- -- CPU Utilization
--- cpu_util = awful.widget.watch('bash -c "~/.config/awesome/Scripts/cpu_util.sh"', 2, function(widget, stdout)
---     widget:set_markup(markup.fontfg(std_font, blue, stdout))
---     return
--- end)
